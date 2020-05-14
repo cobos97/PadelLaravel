6fdf2922f0a63e5cbb6051a767dba636cdd74cbc
Antonio Miguel Cobos Fernandez <cobosmoriles@gmail.com>
2020-05-14 13:03:39 +0200

    Cambiada una ruta de error

--- resources/views/penalizado.blade.php
+++ resources/views/penalizado.blade.php
@@ -6,6 +6,6 @@
 
     <div class="alert alert-danger">Tu usuario a sido penalizado por uno de los administradores, no tendrás acceso a
         los complejos hasta la siguiente fecha: {{date('H:i d/m/Y', Auth()->user()->penalizacion)}}.
-        Si tienes alguna duda puedes contactar con los administradores <a href="{{route('contacta')}}">aquí</a>.
+        Si tienes alguna duda puedes contactar con los administradores <a href="{{route('chatAdmin')}}">aquí</a>.
     </div>
 @endsection
